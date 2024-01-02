c6f02898e1f652a3e8c6a32c7f4d898fca070bbb
IlliaPopovskyi <83787885+IlliaPopovskyi@users.noreply.github.com>
2024-01-02 22:48:27 +0200

    Revert "auto lambda"

--- src/modules/healthy/healthy.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('healthy')
-export class HealthyController {
-  @Get('ping')
-  pong() {
-    return 'pong';
-  }
-}
--- src/modules/healthy/healthy.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HealthyController } from './healthy.controller';
-
-@Module({
-  controllers: [HealthyController]
-})
-export class HealthyModule {}
