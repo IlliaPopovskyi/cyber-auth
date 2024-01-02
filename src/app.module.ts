8e2f0f1d447023b5c4d0f4abc5f8211a405949f2
IlliaPopovskyi <83787885+IlliaPopovskyi@users.noreply.github.com>
2024-01-02 22:47:20 +0200

    Revert "Dev"

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import { AbstractModule } from 'modules/abstract/abstract.module';
 import { ConfigModule } from '@nestjs/config';
 import { RedisModule } from './modules/redis/redis.module';
 import { getEnvPath } from 'common/helpers/env.helper';
-import { HealthyModule } from './modules/healthy/healthy.module';
 
 const envFilePath: string = getEnvPath(`${__dirname}/../..`);
 
@@ -33,14 +32,12 @@ const envFilePath: string = getEnvPath(`${__dirname}/../..`);
             path: '/auth',
             module: AuthModule,
           },
-          { path: '/healthy', module: HealthyModule },
         ],
       },
     ]),
     UserModule,
     AuthModule,
     RedisModule,
-    HealthyModule,
   ],
 })
 export class AppModule {}
